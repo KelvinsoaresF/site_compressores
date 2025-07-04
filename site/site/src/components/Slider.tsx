810cdc0a74b9a13b28140bf0c29c374b7e8a08f6
kelvin Felix  Soares da Silva <163193906+KelvinsoaresF@users.noreply.github.com>
2025-07-04 08:30:19 -0300

    component slider

--- site/site/src/components/Slider.tsx
+++ site/site/src/components/Slider.tsx
@@ -1,17 +1,32 @@
 'use client'
 
 import Image from "next/image";
+import { useState } from "react";
 
-const images = [
-    "/img/img1.jpg",
-    "/img/img2.jpg",
-    "/img/img3.jpg",
-]
 
 export default function Slider() {
+
+    const images = [
+        "/img/img1.jpg",
+        "/img/img2.jpg",
+        "/img/img3.jpg",
+    ]
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+
     return (
-        <div className=" bg-red-500 relative w-full h-[500px] overflow-hidden">
-            <h1>teste</h1>
+        <div className=" bg-red-500 relative w-full h-[600px] overflow-hidden">
+            {images.map((src, index) => (
+                <>
+                    <div
+                        key={index}
+                        className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${index === currentIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'
+                            }`}
+                    >
+
+                    </div>
+                </>
+            ))}
         </div>
     )
 }
