1d2f4539bf3db7e2f8a14f45cb360c4c41ebea09
kelvin Felix  Soares da Silva <163193906+KelvinsoaresF@users.noreply.github.com>
2025-10-20 11:17:26 -0300

    .

--- site/src/app/infoProduct/[id]/page.tsx
+++ site/src/app/infoProduct/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ClientPage from "./ClientPage"
 
-export default function Page({ params }: { params: { id: string } }) {
-  // Apenas repassa a string
+// Tornar a page ASYNC evita conflito de tipagem no Next 15
+export default async function Page({ params }: { params: { id: string } }) {
+  // Apenas repassa a string para o Client Component
   return <ClientPage id={params.id} />
 }
